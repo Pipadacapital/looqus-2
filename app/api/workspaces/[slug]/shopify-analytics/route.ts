799f38e3d6e0c3a9b7e3edfc728c84f638e688fe
Aditya Yaduvanshi <aditya97y@gmail.com>
2026-02-27 06:46:06 +0530

    added

--- app/api/workspaces/[slug]/shopify-analytics/route.ts
+++ app/api/workspaces/[slug]/shopify-analytics/route.ts
@@ -10,7 +10,7 @@ export async function GET(
   const {
     data: { user },
   } = await supabase.auth.getUser()
-
+// s
   if (!user) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
   }
